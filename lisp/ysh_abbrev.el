a8ae492a3434258d5b32c5e352036ec471d64b24
yshalenyk <yshalenyk@gmail.com>
2020-03-21 19:01:17 +0200

    Update abbrev tables

--- lisp/ysh_abbrev.el
+++ lisp/ysh_abbrev.el
@@ -4,290 +4,266 @@
 ;; ;; personal abbrevs
 ;; ;; stolen for xah or ported from snippets 〈Using Emacs Abbrev Mode for Abbreviation〉 http://ergoemacs.org/emacs/emacs_abbrev_mode.html
 
+(defconst terraform-expand-list
+  '(
+    ("data" "data \"\" \"\" {\n  \n}" (7 10 14))
+    ("prov" "provider \"\"  {\n  \n}" (11 18))
+    ("res" "resource \"\" \"\" {\n  \n}" (11 14 19))
+    ("var" "variable \"\" {\n  type = \n}" (11 24))
+    ("out" "output \"\" {\n  value = \n}" (9 23))
+    ("mod" "module \"\" {\n  source = \"\"\n}" (9 25))
+    ("loc" "locals {\n\n}" 10)
+    )
+  "Terraform mode abbrevs"
+  )
+
+(define-abbrev-table 'css-mode-abbrev-table
+  '(
+    ("bg" "background-color: $;")
+    ("bor" "border: $;")
+    ("ff" "font-family: $;")
+    ("fs" "font-size: $;")
+    ))
+
+
+(defconst python-expand-list
+  '(
+    ("pri" "print()" 7)
+    ("true" "True")
+    ("false" "False")
+    ("ret" "return " 8)
+    ("arg" "parser.add_argument(, )" (21 23))
+    ("ass" "assert " 8)
+    ("cls" "class ():\n''''''\n    def __init__(self):\n''''''" (7  8 14 39 45))
+    ("def" "def ():\n''''''\n" (5 6 12 17))
+    ("if" "if :\n    " (4 9))
+    ("ife" "if :\n    \nelse:\n    " (4 9 21))
+    ("ifmain" "if __name__ == '__main__':\n    " 29)
+    ("imp" "import " 8)
+    ("from" "from  import " (6 14))
+    ("pdb" "import pdb; pdb.set_trace()")
+    ("ipdb" "import ipdb; ipdb.set_trace()")
+    ("lc" "[ for  in ]" (2 7 11))
+    ("dc" "{ for  in }" (2 7 11))
+    ("lci" "[ for  in  if ]" (2 7 11 15))
+    ("lci" "{ for  in  if }" (2 7 11 15))
+    ("log" "logger = logging.getLogger()" 28)
+    ("for" "for  in :\n    " (5 9 15))
+    ("se" "self." 6)
+    ("sa" "self. = " (6 9))
+    ("_utf" "# -*- coding: utf-8 -*-");; TODO:
+    ("wh" "while :\n    " (7 13))
+    ("wi" "with :\n    " (6 13))
+    ("wia" "with  as :\n    " (6 10 17))
+    ("setup"
+     (concat
+      "from setuptools import setup\n\n"
+      "package = ''\n"
+      "version = ''\n\n"
+      "setup(name=package,\n"
+      "      version=version,\n"
+      "      description='',\n"
+      "      url='')"
+      )
+     (44 58)) ;; TODO:
+    ("try" "try:\n    \nexcept  as e:\n    " (9 18 28))
+    ("deft" "def test_():\n    " (10 11 16))
+    ("lambda" "lambda :" 8)
+    (
+     "pargs"
+     "def parse_arguments():\n    parser = argparse.ArgumentParser(description='')\n    return parser.parse_args()"
+     74
+     )
+    )
+  "Python mode expansions")
+
+(defconst c-expand-list
+  '(("if" "if () {\n \n} else {\n \n}" (5 10 21))
+    ("ifn" "if () {}" (5 8))
+    ("uns" "unsigned ")
+    ("ins" "#include \"\"" 11)
+    ("inc" "#include <>" 11)
+    ("for" "for(; ; ) {\n\n}" (5 7 9 13))
+    ("switch" "switch () {\n\n}" (9 13))
+    ("case" "case :\n\nbreak;\n" (6 8 16))
+    ("do" "do {\n\n} while ();" (6 16))
+    ("while" "while () {\n\n}" (8 12))
+    ("str" "struct  {}" (8 10))
+    ("default" "default:\n\nbreak;" 10)
+    ("main" "int\nmain(int argc, char * argv[])\n{\n\n}\n" 37))
+  "Expansions for C mode")
+
+
+(defconst rust-expand-list
+  '(
+    ("allow" "#[allow()]" 9)
+    ("ass" "assert!();" 9)
+    ("asse" "assert_eq!();" 12)
+    ("cfg" "#[cfg()]" 7)
+    ("cfgv" "#[cfg( = \"\")]" (7 11))
+    ;; TODO: || doesnt expand
+    ("lam" "|| {\n\n}" (2 6))
+    ("deri" "#[derive()]" 10)
+    (
+     "display"
+     "impl Display for  {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, \"\")
+    }
+}"
+     (18 99)
+     )
+    ("enum" "enum  {\n\n}" (6 9))
+    ("epr" "eprint!(\"\", );" (10 13))
+    ("eprl" "eprintln!(\"\", );" (12 15))
+    ("fn" "fn () ->  {\n\n}" (4 5 10 13))
+    ("fnn" "fn () {\n\n}" (4 5 9))
+    ("fnt" "fn <T>(x: T) where T: Clone-> _ {\n}" (4))
+    ("for" "for  in  {\n\n}" (5 9 12))
+    ("from" "impl From<> for  {
+    fn from(source: ) -> Self {
+       Self {}
+    }
+}
+"
+     (11 17 40 65)
+     )
+    ("fromstr" "impl FromStr for  {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self{})
+    }
+}
+"
+     (18 114)
+     )
+
+    ("if" "if  {\n\n} else {\n\n}" (4 7 17))
+    ("ifn" "if  {\n\n}" (4 7))
+    ("ifl" "if let  =  {\n\n}" (8 11 14))
+    ("impl" "impl  {\n\n}" (6 9))
+    ("implf" "impl  for  {\n\n}" (6 11 14))
+    ("let" "let  = ;" (5 8))
+    ("letm" "let mut  = ;" (9 12))
+    ("loop" "loop {\n\n}" 8)
+    ("macro" "macro_rules!  {
+     () => ();
+}"
+     (14 23 29)
+     )
+    ("main" "fn main() {\n\n}" 13)
+    ("match" "match  {\n\n}" (7 10))
+    ("new" "pub fn new() -> Self {\n\n}" (12 17 20))
+    ("p" "pub " 5)
+    ("pr" "print!(\"\", );" (9 12))
+    ("prl" "println!(\"\", );" (11 14))
+    ("res" "Result<, failure::Error>" 8)
+    ("resu" "Result<, failure::Error>" 8)
+    ("ok" "Ok()" 4)
+    ("some" "Some()" 6)
+    ("err" "Err()" 5)
+    ("static" "static :  = ;" (8 10 13))
+    ("struct" "struct  {\n\n}" (8 11))
+    ("test" "#[test]\nfn test_() {\n\n}" (17 22))
+    ("testm" "#[cfg(test)]\nmod  {\n    use super::*;\n\n}" (18 39))
+    ("trait" "trait  {\n\n}" (7 10))
+    ("type" "type  = ;" (6 9))
+    ("union" "union  {\n\n}" (7 10))
+    ("warn" "#[warn()]" 8)
+    ("whl" "while  {\n\n}" (7 10))
+    ("whll" "while let  =  {\n\n}" (11 14 17))
+    )
+  "expand list for rust")
+
+(defconst emacs-lisp-expand
+  (list
+   (list
+    "defu"
+    (concat
+     "(defun   ()\n"
+     "  \"\"\n"
+     "  (interactive)\n"
+     "  (let* (\n"
+     "         )\n"
+     "    \n"
+     "    ))")
+    (list 8 11 16 32 43 59))
+
+   (list
+    "defs"
+    (concat
+     "(defsubst   ()\n"
+     "  \"\"\n"
+     "  (interactive)\n"
+     "  )")
+    (list 11 14 19 23 39))
+
+   (list
+    "defm"
+    (concat
+     "(defmacro  ()\n"
+     "  \"\"\n"
+     "  `( \n"
+     "    ))")
+    (list 11 13 18 25))
+
+   (list
+    "defa"
+    (concat
+     "(defadvice   (around   act)\n"
+     "  \"\"\n"
+     "  \n"
+     "  )")
+    (list 12 22 32 36))
+
+   (list
+    "defc"
+    "(defconst   nil\n  \"\")\n"
+    (list 11 13 20))
+
+   (list
+    "defv"
+    "(defvar   nil\n  \"\")\n"
+    (list 9 11 18))
+
+   (list
+    "let"
+    "(let* (\n)\n    "
+    (list 8 13))
+
+   (list
+    "sav"
+    "(save-excursion\n \n)"
+    (list 18))
+
+   (list
+    "aut"
+    "(autoload ' \"\" t t)\n"
+    (list 12 14))
+
+   )
+  "Expansions for Lisp mode.  See `expand-add-abbrevs'.")
+
 
 (use-package abbrev
-  :custom
-  (abbrev-file-name "~/.emacs.d/abbrevs")
-  (save-abbrevs 'silently)
+  :demand
   :config
-  (abbrev-mode t))
+  (setq abbrev-file-name "~/.emacs.d/abbrev_defs")
+  (setq save-abbrevs 'silently)
+  (define-key xah-fly-e-keymap (kbd "j") 'expand-jump-to-previous-slot)
+  (define-key xah-fly-e-keymap (kbd "k") 'expand-jump-to-next-slot)
+  (add-hook 'expand-expand-hook 'indent-according-to-mode)
+  (add-hook 'expand-jump-hook 'indent-according-to-mode)
+  (setq-default abbrev-mode t)
+  )
 
-;; (define-abbrev-table 'org-mode-abbrev-table
-;;   '(
-;;     ("<au" "#+author: $")
-;;     ("<c" "#+begin_center\n$\n#+end_center")
-;;     ("<da" "#+date: $")
-;;     ("desc" "#+description: $")
-;;     ("<ti" "#+title $")
-;;     ("<q" "#+begin_quote\n$\n#+end_quote")
-;;     ("<py" "#+begin_src python\n$\n#+end_src")
-;;     ("<li" "[[$][description]")
-;;     ("<ke" "#+keywords: $")
-;;     ))
-;; (define-abbrev-table 'terraform-mode-abbrev-table
-;;   '(
-;;     ("data" "data \"$\" \"\" {\n}")    ("data" "data \"$\" \"\" {\n}")
-;;     ("prov" "provider \"$\"  {\n}")
-;;     ("res" "resource \"$\" \"\" {\n}")
-;;     ("var" "variable \"$\" {\n  default = \"\"\n}")
-;;     ("out" "output \"$\" {\n  value = \"\"\n}")
-;;     ("mod" "module \"$\" {\n  source = \"\"\n}")
-;;     ("loc" "locals {\n$\n}")
-;;     ))
-
-;; (define-abbrev-table 'css-mode-abbrev-table
-;;   '(
-;;     ("bg" "background-color: $;")
-;;     ("bor" "border: $;")
-;;     ("ff" "font-family: $;")
-;;     ("fs" "font-size: $;")
-;;     ))
-
-;; (define-abbrev-table 'c-mode-abbrev-table
-;;   '(
-;;     ("case" "case $; break;")
-;;     ("do" "do { } while ($)")
-;;     ("if" "if ($) {}")
-;;     ("els" "else {}")
-;;     ("ifel" "if ($) {} else {}")
-;;     ("for" "for (int i = 0; i < N; i++) {$}")
-;;     ("str" "struct $ {}")
-;;     ("switch" "switch ($) {}")
-;;     ("wh" "while ($) {}")
-;;     ("inc" "#include <$>")
-;;     ("ins" "#include \"$\"")
-;;     ("def" "#define $")
-;;     ("main" "int main(int argc, char *argv[]) {$}")
-;;     ))
-
-;; (define-abbrev-table 'python-mode-abbrev-table
-;;   '(
-;;     ("p" "print($)")
-;;     ("r" "return")
-;;     ("arg" "parser.add_argument('$', '')")
-;;     ("ass" "assert")
-;;     ("cls" "class $():\n    ''' '''\n    def __init__():\n    ''' '''")
-;;     ("def" "def $():\n    ''''''")
-;;     ("if" "if $:")
-;;     ("ife" "if $:\nelse:")
-;;     ("ifmain" "if __name__ == '__main__':")
-;;     ("imp" "import $")
-;;     ("from" "from $ import ")
-;;     ("pdb" "import pdb; pdb.set_trace()")
-;;     ("ipdb" "import ipdb; ipdb.set_trace()")
-;;     ("lam" "lambda $:")
-;;     ("li" "[item for item in $]")
-;;     ("log" "logger = logging.getLogger($)")
-;;     ("for" "for $ in :")
-;;     ("s" "self.$")
-;;     ("sa" "self.$ = ")
-;;     ("_utf" "# -*- coding: utf-8 -*-")
-;;     ("wh" "while $:")
-;;     ("wi" "with $:")
-;;     ("setup" "from setuptools import setup\n\npackage = '$'\nversion = ''\n\nsetup(name=package,\n      version=version,\n      description='',\n      url='')")
-;;     ("try" "try:\n    $\nexcept :")
-;;     ("dt" "def test_$():\n")
-;;     ("lambda" "lambda $:")
-;;     ("pargs" "def parse_arguments():\n    parser = argparse.ArgumentParser(description='$')\n    return parser.parse_args()")
-;;     ;;
-;;     ))
-
-;; (define-abbrev-table 'rust-mode-abbrev-table
-;;   '(
-;;     ("allow" "#[allow($)]")
-;;     ("ass" "assert!($);")
-;;     ("asseq" "assert_eq!($);")
-;;     ("case" "$ => {},")
-;;     ("cfg" "#[cfg($)]")
-;;     ("cfgv" "#[cfg($ = \"\")]")
-;;     ;; TODO: || doesnt expand
-;;     ("||" "|$|{\n}")
-;;     ("derive" "#[derive($)]")
-;;     ("display" "impl Display for $ {
-;;     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-;;         write!(f, \"\")
-;;     }
-;; }")
-;;     ("drop" "impl Drop for $ {
-;;      fn drop(&mut self) {
-
-;;       }
-;; }")
-;;     ("enum" "enum $ {\n}")
-;;     ("eprint" "eprint!(\"$\", );")
-;;     ("eprintln" "eprintln!(\"$\", );")
-;;     ("fn" "fn $() {\n}")
-;;     ("fnr" "fn $() -> _ {\n}")
-;;     ("fnt" "fn $<T>(x: T) where T: Clone-> _ {\n}")
-;;     ("for" "for $ in  {\n}")
-;;     ("from" "impl From<$> for  {
-;;     fn from(source: ) -> Self {
-;;        Self { }
-;;     }
-;; }
-;; ")
-;;     ("fromstr" "impl FromStr for $ {
-;;     type Err = Error;
-
-;;     fn from_str(s: &str) -> Result<Self, Self::Err> {
-;;         Ok(Self{})
-;;     }
-;; }
-;; ")
-;;     ("if" "if $ {\n}")
-;;     ("ife" "if $ {\n} else {\n}")
-;;     ("ifl" "if let $ = {\n}")
-;;     ("impl" "impl $ {\n}")
-;;     ("implt" "impl $ for T {\n}")
-;;     ("let" "let $ = ;")
-;;     ("letm" "let mut $ = ;")
-;;     ("loop" "lopp {\n    $\n}")
-;;     ("macro" "macro_rules! $ {
-;;      () => ();
-;; }")
-;;     ("main" "fn main() {\n    $\n}")
-;;     ("match" "match $ {\n}")
-;;     ("new" "pub fn new($) ->  {
-;;     { }
-;; }")
-;;     ("p" "pub$")
-;;     ("print" "print!(\"\", $)")
-;;     ("println" "println!(\"\", $)")
-;;     ("result" "Result<$, failure::Error>")
-;;     ("static" "static $: = ;")
-;;     ("struct" "struct $ {\n}")
-;;     ("test" "#[test]
-;; fn test_$() {
-
-;; }")
-;;     ("testmod" "#[cfg(test)]
-;; mod $ {
-;;     use super::*;
-
-;;     #[test]
-;;     fn () {
-;;     }
-;; }")
-;;     ("trait" "trait $ {\n}")
-;;     ("type" "type $ = ;")
-;;     ("union" "union $ {\n}")
-;;     ("warn" "#[warn($)]")
-;;     ("while" "while $ {\n}")
-;;     ("whilel" "while let $ =  {\n}")
-;;     ))
+(expand-add-abbrevs terraform-mode-abbrev-table terraform-expand-list)
+(expand-add-abbrevs python-mode-abbrev-table python-expand-list)
+(expand-add-abbrevs emacs-lisp-mode-abbrev-table emacs-lisp-expand )
+(expand-add-abbrevs rust-mode-abbrev-table rust-expand-list)
+(expand-add-abbrevs c-mode-abbrev-table c-expand-list)
 
 
-;; (define-abbrev-table 'emacs-lisp-mode-abbrev-table
-;;   '(
-;;     ("adh" "(add-hook '$ ')")
-;;     ("a" "(and $)")
-;;     ("bc" "(backward-char $)")
-;;     ("bol" "(beginning-of-line)")
-;;     ("bfn" "(buffer-file-name)")
-;;     ("bpm" "(buffer-modified-p $)")
-;;     ("cond" "(cond\n($))")
-;;     ("const" "(defconst $ \"docstring\")")
-;;     ("cb" "(current-buffer)")
-;;     ("defalias" "(defalias '$ ' \"docstring\")")
-;;     ("defcustom" "(defcustom '$ ' \"docstring\" args)")
-;;     ("defk" "(define-key $-mode-map (kbd \"\") )")
-;;     ("def" "(defun $ ()\n  \"docstring\"\n  ())")
-;;     ("defvar" "(defvar $ \"\" \"docstring\")")
-;;     ("dc" "(delete-char $)")
-;;     ("dr" "(delete-region $)")
-;;     ("eol" "(end-of-line)")
-;;     ("error" "(error $)")
-;;     ("deft" "(ert-deftest $ ()\n)")
-;;     ("efn" "(expand-file-name $)")
-;;     ("f" "(format $ \"\")")
-;;     ("gsk" "(global-set-key (kbd \"$\") )")
-;;     ("lam" "(lambda ($) ())")
-;;     ("lt" "(let ($)\n)")
-;;     ("lts" "(let* ($)\n)")
-;;     ("sh" "(should $)")
-;;     ("u" "(unless $)")
-;;     ("up" "(use-package $ :ensure t)")
-;;     ("w" "(when $)")
-;;     ))
-
-;; (set-default 'abbrev-mode t)
-
-;; (defun xah-abbrev-h-f ()
-;;   "Abbrev hook function, used for `define-abbrev'.
-;;  Our use is to prevent inserting the char that triggered expansion. Experimental.
-;;  the “h-f” stand for hook function.
-;; Version 2016-10-24"
-;;   t)
-
-;; (put 'xah-abbrev-h-f 'no-self-insert t)
-
-;; (setq abbrev-expand-function 'xah-global-expand-abbrev)
-
-;; (defun xah-global-expand-abbrev ()
-;;   "function for value of `abbrev-expand-function'.
-;; Expand the symbol before cursor,
-;; if cursor is not in string or comment.
-;; Returns the abbrev symbol if there's a expansion, else nil.
-;; Version 2019-01-10"
-;;   (interactive)
-;;   (let ( $p1 $p2
-;; 	     $abrStr
-;; 	     $abrSymbol
-;; 	     )
-
-;;     ;; (save-excursion
-;;     ;;   (forward-symbol -1)
-;;     ;;   (setq $p1 (point))
-;;     ;;   (goto-char $p0)
-;;     ;;   (setq $p2 $p0))
-
-;;     (save-excursion
-;;       ;; 2017-01-16 note: we select the whole symbol to solve a problem. problem is: if “aa”  is a abbrev, and “▮bbcc” is existing word with cursor at beginning, and user wants to type aa- to result in aa-bbcc. Normally, aa immediately expands. This prevent people editing bbcc to become aa-bbcc. This happens for example in elisp, when editing “search-forward” to become “re-search-forward”. The downside of this is that, people cannot type a abbrev when in middle of a word.
-;;       (forward-symbol -1)
-;;       (setq $p1 (point))
-;;       (forward-symbol 1)
-;;       (setq $p2 (point)))
-
-;;     (setq $abrStr (buffer-substring-no-properties $p1 $p2))
-;;     (setq $abrSymbol (abbrev-symbol $abrStr))
-;;     (if $abrSymbol
-;; 	(progn
-;; 	  (abbrev-insert $abrSymbol $abrStr $p1 $p2 )
-;; 	  (reindent-then-newline-and-indent)
-;; 	  (xah-global-abbrev-position-cursor $p1)
-;; 	  $abrSymbol)
-;;       nil)))
-
-;; ;; (defun delete-preceding-space-before-expand ()
-;; ;;   (when (and (= (char-after) ?\s)
-;; ;;              ;; only do if expanded with space
-;; ;;              (= last-command-event ?\s)
-;; ;;              (abbrev--before-point))
-;; ;;     (message "%S" last-command-event)
-;; ;;     (delete-char 1)))
-
-;; ;; (add-hook 'pre-abbrev-expand-hook 'delete-preceding-space-before-expand)
-
-
-;; (defun clean-previous-symbol ()
-;;   "Cleans prev space"
-;;   (interactive)
-;;   (delete-char 1)
-;;   (when (char-equal (char-before) ?\s)
-;;     (delete-backward-char 1)))
-
-;; (defun xah-global-abbrev-position-cursor (&optional @pos)
-;;   "Move cursor back to $ if exist, else put at end.
-;; Return true if found, else false.
-;; Version 2016-10-24"
-;;   (interactive)
-;;   (let (($found-p (search-backward "$" (if @pos @pos (max (point-min) (- (point) 100))) t )))
-;;     (when $found-p (clean-previous-symbol))
-;;     $found-p
-;;     ))
-
-
-;; (setq save-abbrevs nil)
 (provide 'ysh_abbrev)
-
 ;; ;;; ysh_abbrev.el ends here
