32f523864598c846b8c40003080914c8f8e4f2d7
yshalenyk <yshalenyk@gmail.com>
2021-04-18 18:27:51 +0300

    add python-pytest

--- lisp/ysh_python.el
+++ lisp/ysh_python.el
@@ -9,6 +9,7 @@
   :init
   (advice-add 'python-mode :before 'elpy-enable))
 
+(use-package python-pytest :ensure t)
 ;; (use-package anaconda-mode
 ;;   :ensure t
 ;;   :config
@@ -19,19 +20,19 @@
 ;;     (add-hook 'python-mode-hook 'anaconda-mode)
 ;;     (add-hook 'python-mode-hook 'anaconda-eldoc-mode)))
 
-(use-package pyenv-mode
-  :ensure t
-  :hook (python-mode . pyenv-mode)
-  :config
-  (defun projectile-pyenv-mode-set ()
-    "Set pyenv version matching project name."
-    (let ((project (projectile-project-name)))
-      (if (member project (pyenv-mode-versions))
-          (pyenv-mode-set project)
-	(pyenv-mode-unset))))
-  (add-hook 'projectile-after-switch-project-hook 'projectile-pyenv-mode-set))
+;; (use-package pyenv-mode
+;;   :ensure t
+;;   :hook (python-mode . pyenv-mode)
+;;   :config
+;;   (defun projectile-pyenv-mode-set ()
+;;     "Set pyenv version matching project name."
+;;     (let ((project (projectile-project-name)))
+;;       (if (member project (pyenv-mode-versions))
+;;           (pyenv-mode-set project)
+;; 	(pyenv-mode-unset))))
+;;   (add-hook 'projectile-after-switch-project-hook 'projectile-pyenv-mode-set))
 
-(use-package pyenv-mode-auto
-  :ensure t)
+;; (use-package pyenv-mode-auto
+;;   :ensure t)
 
 (provide 'ysh_python)
