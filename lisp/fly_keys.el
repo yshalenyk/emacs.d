db320fc5f7256602f21cd9ca646fd26e623ba915
yshalenyk <yshalenyk@gmail.com>
2021-04-18 18:26:50 +0300

    set xah-fly-M-x command

--- lisp/fly_keys.el
+++ lisp/fly_keys.el
@@ -7,9 +7,13 @@
   (setq-default xah-fly-use-meta-key nil)
   :config
   (xah-fly-keys-set-layout "dvorak")
+  (setq xah-fly-M-x-command 'counsel-M-x)
   (global-set-key  (kbd "M-u") 'xah-fly-command-mode-activate-no-hook)
   (global-set-key  (kbd "M-'") 'keyboard-quit)
-  (xah-fly-keys 1))
+  (xah-fly-keys 1)
+  (define-key xah-fly-w-keymap (kbd "n") 'make-frame)
+  (define-key xah-fly-w-keymap (kbd "j") 'other-frame)
+  )
 
 ;; (global-set-key (kbd "ESC") 'xah-fly-command-mode-activate)
 (provide 'fly_keys)
