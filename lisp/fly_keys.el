81938c1656dcf29ef94afea8a45dc7ec508537c4
yshalenyk <yshalenyk@gmail.com>
2022-12-14 23:46:52 +0200

    update flykeys source

--- lisp/fly_keys.el
+++ lisp/fly_keys.el
@@ -1,7 +1,7 @@
-
+""
 (use-package xah-fly-keys
-  :ensure t
   :demand t
+  :quelpa (xah-fly-keys :fetcher git :url "https://github.com/xahlee/xah-fly-keys")
   :init
   (setq-default xah-fly-use-control-key nil)
   (setq-default xah-fly-use-meta-key nil)
@@ -13,6 +13,9 @@
   (xah-fly-keys 1)
   (define-key xah-fly-w-keymap (kbd "n") 'make-frame)
   (define-key xah-fly-w-keymap (kbd "j") 'other-frame)
+  (define-key xah-fly-c-keymap (kbd ".") 'counsel-find-file)
+  (define-key xah-fly-c-keymap (kbd "h") 'counsel-recentf)
+  ;; (define-key xah-fly-leader-key-map (kbd "e") 'lsp-keymap-prefix)
   )
 
 ;; (global-set-key (kbd "ESC") 'xah-fly-command-mode-activate)
