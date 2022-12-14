398bf3c963fa6135067b952fa1946da60e83a475
yshalenyk <yshalenyk@gmail.com>
2022-12-14 23:31:01 +0200

    disable semantic

--- lisp/ysh_semantic.el
+++ lisp/ysh_semantic.el
@@ -1,24 +1,24 @@
 (require 'semantic)
 
-(global-semanticdb-minor-mode 1)
-(global-semantic-idle-scheduler-mode 1)
-(global-semantic-idle-summary-mode 1)
-(global-semantic-stickyfunc-mode 1)
-(global-semanticdb-minor-mode 1)
-(semantic-mode 1)
+;; (global-semanticdb-minor-mode 1)
+;; (global-semantic-idle-scheduler-mode 1)
+;; (global-semantic-idle-summary-mode 1)
+;; (global-semantic-stickyfunc-mode 1)
+;; (global-semanticdb-minor-mode 1)
+;; (semantic-mode 1)
 
 
 (require 'ede)
-(global-ede-mode t)
+;; (global-ede-mode t)
 
 (require 'ede/generic)
 (require 'semantic/ia)
 (require 'semantic/sb)
-(ede-enable-generic-projects)
+;; (ede-enable-generic-projects)
 
 (require 'srecode)
 
-(global-srecode-minor-mode 1)
+;; (global-srecode-minor-mode 1)
 
 
 ;; (use-package ecb
