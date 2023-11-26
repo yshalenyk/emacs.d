7a754cb49ca733b6e08ccce47dad29048de95436
yshalenyk <yshalenyk@gmail.com>
2023-11-26 22:46:52 +0200

    add strait.el bootstrap

--- init.el
+++ init.el
@@ -63,6 +63,23 @@
                          ("melpa"     . "https://melpa.org/packages/")
                          ;;("marmalade" . "https://marmalade-repo.org/packages/")
                          ))
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 (package-initialize)
 
 
